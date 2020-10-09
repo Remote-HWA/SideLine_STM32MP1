af84d3586ea61bcdcd4fbdec28397c75c881b160
joseph gravellier <joseph.gravellier@gmail.com>
2020-10-09 18:56:02 +0200

    hierarchy changed

--- CortexM4_code/Debug/Application/startup/syscalls.d
+++
@@ -1,1 +0,0 @@
-Application/startup/syscalls.o: ../Application/startup/syscalls.c
---
+++ make_and_export_code_ide.sh
@@ -0,0 +1,8 @@
+#source /home/joseph/STM32_workspace/STM32MP15-Ecosystem-v1.1.0/Developer-Package/SDK/environment-setup-cortexa7t2hf-neon-vfpv4-openstlinux_weston-linux-gnueabi
+#cd CortexA7_code/
+#make clean
+#make
+#cd ..
+ssh root@192.168.0.15 'rm -r SideLine; mkdir SideLine'
+scp CortexA7_code/cortex-A7.elf root@192.168.0.15:/home/root/SideLine/cortex-A7.elf
+scp /home/joseph/STM32CubeIDE/workspace_1.4.0/SideLine/CM4/Debug/SideLine_CM4.elf root@192.168.0.15:/home/root/SideLine/cortex-M4.elf
